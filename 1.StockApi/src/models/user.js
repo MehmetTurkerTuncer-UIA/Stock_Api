1822f2df36844e5e565fee53ee71a3941226dd53
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-21 12:10:26 +0200

    usersModel

--- 1.StockApi/src/models/user.js
+++ 1.StockApi/src/models/user.js
@@ -4,3 +4,74 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        index: true,
+
+    },
+    
+    password: {
+            type: String,  
+            trim: true,      
+            required: true,
+
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        index: true,    
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    isStaff: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+
+},
+    
+    
+    
+    
+    {
+
+        collection: 'users',
+        timestamps: true,   
+
+    })
+   
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------- */
