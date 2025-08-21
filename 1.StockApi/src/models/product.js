be325b94a45cedf10c7ed555b530386279636545
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-21 14:40:37 +0200

    productModelControllerRouter

--- 1.StockApi/src/models/product.js
+++ 1.StockApi/src/models/product.js
@@ -4,3 +4,37 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const productSchema = new mongoose.Schema({
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'categories',
+        required: true  
+    },
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'brands',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true, 
+    },
+    quantity: {
+        type: Number,
+        default: 0,
+    }       
+},
+    
+    
+    
+    {collection: 'products', 
+        
+        timestamps: true   })
+
+
+// EXPORT:        
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('products', productSchema)
+/* ------------------------------------------------------- */        
