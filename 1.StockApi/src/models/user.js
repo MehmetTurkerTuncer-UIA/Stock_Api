b2506fbe3647ff55fe2846886db1c1d4af7814fa
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-21 13:21:38 +0200

    userControllerRoutesIndex

--- 1.StockApi/src/models/user.js
+++ 1.StockApi/src/models/user.js
@@ -1,77 +1,63 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 
-
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        index: true,
-
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
     },
-    
+
     password: {
-            type: String,  
-            trim: true,      
-            required: true,
-
+      type: String,
+      trim: true,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        index: true,    
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
     },
     firstName: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     lastName: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     isActive: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
     isStaff: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     isAdmin: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
+  },
 
-
-},
-    
-    
-    
-    
-    {
-
-        collection: 'users',
-        timestamps: true,   
-
-    })
-   
-
-
-
-
-
-
-
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
 
 /* ------------------------------------------------------- */
+
+module.exports = mongoose.model("User", UserSchema);
--- 1.StockApi/src/routes/user.js
+++ 1.StockApi/src/routes/user.js
@@ -1,6 +1,28 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+
+// routes/user
+const user = require("../controllers/user");
+
+// URL: /users
+
+router
+  .route("/")
+  .get(user.list) // List Users
+  .post(user.create); // Create User
+
+router
+  .route("/:id")
+  .get(user.read) // Read User
+  .put(user.update) // Update User
+  .patch(user.update) // Update User
+  .delete(user.delete); // Delete User
+
+
+//EXPORT
+module.exports = router;
+
