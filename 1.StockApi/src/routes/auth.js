64459e3da3f2d41dbca3369fb101f0287f4c2df3
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-21 21:15:13 +0200

    update

--- 1.StockApi/src/routes/auth.js
+++ 1.StockApi/src/routes/auth.js
@@ -4,3 +4,18 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+
+
+
+const auth = require('../controllers/auth')
+
+// URL: /auth
+
+router.post('/login', auth.login)
+router.post('/refresh', auth.refresh)
+router.post('/logout', auth.logout)
+
+// EXPORTS
+/* ------------------------------------------------------- */
+module.exports =  router
+/* ------------------------------------------------------- */
